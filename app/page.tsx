d63ca7820d33506522051aa7e89c1c6b6a71c5d9
Emon-36 <emon23702036@gmail.com>
2026-04-14 13:51:42 +0600

    feat : main page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { TodoStore } from "@/store/useTodoStore"
 
 export default function Home() {
 
-  const { todos, addTodo } = TodoStore()
+  const { todos, addTodo, deleteTodo, toggleTodo } = TodoStore()
   const [text, setText] = useState('')
   const [category , setCategory] = useState<Category>('personal')
   const [priority, setPriority] = useState<Priority>('medium')
@@ -94,7 +94,45 @@ export default function Home() {
           <button className="px-4 py-2 rounded-xl text-sm font-medium bg-gray-800 text-gray-400 hover:bg-gray-700">Active</button>
           <button className="px-4 py-2 rounded-xl text-sm font-medium bg-gray-800 text-gray-400 hover:bg-gray-700">Completed</button>
         </div>
+        <div className="flex flex-col gap-3">
+  {todos.length === 0 && (
+    <p className="text-center text-gray-600 py-10">No tasks yet. Add one above!</p>
+  )}
+  {todos.map((todo) => (
+    <div
+      key={todo.id}
+      className={`bg-gray-900 rounded-2xl px-5 py-4 flex items-start gap-4 shadow transition ${
+        todo.completed ? 'opacity-50' : ''
+      }`}
+    >
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => toggleTodo(todo.id)}
+        className="mt-1 w-5 h-5 accent-indigo-500 cursor-pointer"
+      />
+      <div className="flex-1">
+        <p className={`font-medium ${todo.completed ? 'line-through text-gray-500' : 'text-white'}`}>
+          {todo.text}
+        </p>
+        <div className="flex gap-3 mt-1 text-xs text-gray-500 flex-wrap">
+          <span className="bg-gray-800 px-2 py-0.5 rounded-full">{todo.category}</span>
+          <span className={`font-semibold ${
+            todo.priority === 'high' ? 'text-red-500' :
+            todo.priority === 'medium' ? 'text-yellow-500' : 'text-green-500'
+          }`}>{todo.priority}</span>
+          {todo.dueDate && <span>📅 {todo.dueDate}</span>}
+        </div>
+      </div>
+          <button
+            onClick={() => deleteTodo(todo.id)}
+             className="text-gray-600 hover:text-red-500 transition text-xl"
+             >✕</button>
+        </div>
+       ))}
+     </div>
 
+       
         
       </div>
     </main>
